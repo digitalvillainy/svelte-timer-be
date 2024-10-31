83b1a12de1ed76fb466063d938c28f54cd045021
rrivera <rrivera@redbannermedia.com>
2024-10-31 05:20:27 -0400

    changed description type to handle long text

--- database/migrations/2024_10_28_065011_create_todos_table.php
+++ database/migrations/2024_10_28_065011_create_todos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->longText('description');
             $table->boolean('completed');
             $table->timestamps();
 
